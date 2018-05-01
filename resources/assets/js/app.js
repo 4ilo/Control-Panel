9c17cac62a352e05b1408de4c27de28e9a698fec
olivier <oliviervandeneede@hotmail.com>
2018-05-01 16:45:40 +0200

    Hide menu on mobile

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,5 +6,15 @@ Vue.component('flash', require('./components/Flash'));
 const app = new Vue({
     el: '#app',
 
-    router: router
+    router: router,
+
+    data: {
+        showMenu: true
+    },
+
+    methods: {
+        toggleMenu() {
+            this.showMenu = !this.showMenu;
+        }
+    }
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,13 @@
 <div class="container" id="app">
     <div class="row marginTop">
         <div class="col-sm-2">
-            <ul class="list-group">
+            <div class="text-center mb-2">
+                <button type="button" @click="toggleMenu" class="btn btn-secondary d-sm-none">
+                    <i class="fa fa-bars"></i>
+                </button>
+            </div>
+
+            <ul class="list-group" :class="{'d-none': showMenu}">
                 <li class="list-group-item"><h4>Dashboard</h4></li>
 
                 <router-link to="/" tag="li" class="list-group-item" exact>
